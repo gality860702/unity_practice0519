25a9d6424b882a87b6daaf9b6d2ef75358558829
gality860702 <63327350+gality860702@users.noreply.github.com>
2020-05-25 11:48:44 +0800

    update0519hw

---
+++ Unity_hw0519/Assets/PRACTICE.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PRACTICE : MonoBehaviour
+{
+    
+    public SpriteRenderer tran1;
+
+    public Rigidbody ball;
+    
+
+
+    void Start()
+    {
+        Time.timeScale = 2;
+        
+        tran1.flipX =true;
+
+        print(Mathf.Floor(123456));
+
+        ball. AddForce(0,500,800);
+    }
+
+   
+}
